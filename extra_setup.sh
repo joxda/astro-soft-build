cca79ca927f32893ec4e85dd8df5974d433d9e02
Joxda <joxda@yahoo.de>
2025-02-15 17:00:44 +0200

    fix versions of indiweb requirements

--- extra_setup.sh
+++ extra_setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-sudo pip install indiweb
+sudo mkdir -p /usr/local/indiweb
+sudo python -m venv /usr/local/indiweb/venv
+sudo /usr/local/indiweb/venv/bin/pip install indiweb requests==2.32.2 psutil bottle==0.12.25 importlib_metadata==8.5.0
 sudo curl https://raw.githubusercontent.com/joxda/indiwebmanager/refs/heads/master/indiwebmanager.service -o /etc/systemd/system/indiwebmanager.service
 sudo chmod 644 /etc/systemd/system/indiwebmanager.service
 
 sudo curl https://raw.githubusercontent.com/joxda/indiwebmanager/refs/heads/master/tahti.rules -o /etc/udev/rules.d/tahti.rules
 
 sudo curl https://raw.githubusercontent.com/joxda/indiwebmanager/refs/heads/master/resetOnstep.py -o /usr/local/bin/resetOnstep.py
-sudo chmod 755 /usr/local/bin/Onstep.py
+sudo chmod 755 /usr/local/bin/resetOnstep.py
 
 
 
