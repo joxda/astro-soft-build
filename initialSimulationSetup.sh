9d380d19fe764cc89e4b3859395071792f9d1d39
Joachim Janz <joxda@yahoo.de>
2025-03-06 18:25:47 +0200

    Create initialSimulationSetup.sh

---
+++ initialSimulationSetup.sh
@@ -0,0 +1,9 @@
+indiwebmanagercli --host localhost --port 8624 create-profile Simulation
+indiwebmanagercli --host localhost --port 8624 add-driver Simulation indi_simulator_telescope
+indiwebmanagercli --host localhost --port 8624 add-driver Simulation indi_simulator_ccd
+indiwebmanagercli --host localhost --port 8624 add-driver Simulation indi_simulator_focuser
+indiwebmanagercli --host localhost --port 8624 add-driver Simulation indi_simulator_guider
+indiwebmanagercli --host localhost --port 8624 set-profile-option Simulation autostart true
+indiwebmanagercli --host localhost --port 8624 set-profile-option Simulation autoconnect true
+indiwebmanagercli --host localhost --port 8624 get-profile Simulation
+indiwebmanagercli --host localhost --port 8624 start-profile Simulation
