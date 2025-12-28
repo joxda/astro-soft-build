8bcc21c9da5e37b61c6b29349359243cbfd9d672
Joachim Janz <joxda@yahoo.de>
2025-12-28 09:01:38 +0000

    sudo chroot also for build more

--- build-more.sh
+++ build-more.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+if [[ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/. 2>/dev/null)" ]]; then
+  IN_CHROOT=1
+else
+  IN_CHROOT=0
+fi
+
+SUDO=()
+(( IN_CHROOT )) || SUDO=(sudo)
+
 
 BUILD_DIR="/usr/local/tahti/"
 ROOTDIR="$BUILD_DIR/repos"
 
-[ ! -d "$ROOTDIR" ] && { sudo mkdir -p "$ROOTDIR" && sudo chown tahti:tahti $BUILD_DIR && sudo chown tahti:tahti $ROOT_DIR; }
+[ ! -d "$ROOTDIR" ] && { "${SUDO[@]}" mkdir -p "$ROOTDIR" && "${SUDO[@]}" chown tahti:tahti $BUILD_DIR && "${SUDO[@]}" chown tahti:tahti $ROOT_DIR; }
 cd "$ROOTDIR"
 
 
@@ -12,24 +21,24 @@ cd "$ROOTDIR"
 cd gpspanel
 git fetch origin
 git pull
-sudo /usr/local/tahti/venv/bin/pip install -r requirements.txt
-[ ! -d "/var/www/gpspanel" ] && { sudo mkdir /var/www/gpspanel; }
-sudo cp -r ./* /var/www/gpspanel/
+"${SUDO[@]}" /usr/local/tahti/venv/bin/pip install -r requirements.txt
+[ ! -d "/var/www/gpspanel" ] && { "${SUDO[@]}" mkdir /var/www/gpspanel; }
+"${SUDO[@]}" cp -r ./* /var/www/gpspanel/
 
 cd "$ROOTDIR"
 [ ! -d "astropanel" ] && { git clone https://github.com/joxda/astropanel.git || { echo "Failed to clone astropanel"; exit 1; } }
 cd astropanel
 git fetch origin
 git pull
-sudo /usr/local/tahti/venv/bin/pip install -r requirements.txt
-[ ! -d "/var/www/astrospanel" ] && { sudo mkdir /var/www/astropanel; } 
-sudo cp	-r ./* /var/www/astropanel/
+"${SUDO[@]}" /usr/local/tahti/venv/bin/pip install -r requirements.txt
+[ ! -d "/var/www/astrospanel" ] && { "${SUDO[@]}" mkdir /var/www/astropanel; } 
+"${SUDO[@]}" cp	-r ./* /var/www/astropanel/
 
 cd "$ROOTDIR"
 [ ! -d "astroberry-server-wui" ] && { git clone -b j3 --single-branch https://github.com/joxda/astroberry-server-wui.git || { echo "Failed to clone astroberry-server-wui"; exit 1; } }
 cd astroberry-server-wui
 git fetch
 git pull
-sudo cp -r files/html/* /var/www/html/
+"${SUDO[@]}" cp -r files/html/* /var/www/html/
 
 
