3ad395889cf095681af897fb2b6d40f71d776003
joxda <joxda@yahoo.de>
2026-01-05 22:45:49 +0200

    add gpsd

--- initialSimulationSetup.sh
+++ initialSimulationSetup.sh
@@ -3,6 +3,7 @@ indiwebmanagercli --host localhost --port 8624 add-driver Simulation indi_simula
 indiwebmanagercli --host localhost --port 8624 add-driver Simulation indi_simulator_ccd
 indiwebmanagercli --host localhost --port 8624 add-driver Simulation indi_simulator_focuser
 indiwebmanagercli --host localhost --port 8624 add-driver Simulation indi_simulator_guider
+indiwebmanagercli --host localhost --port 8624 add-driver Simulation indi_gpsd
 indiwebmanagercli --host localhost --port 8624 set-profile-option Simulation autostart true
 indiwebmanagercli --host localhost --port 8624 set-profile-option Simulation autoconnect true
 indiwebmanagercli --host localhost --port 8624 get-profile Simulation
